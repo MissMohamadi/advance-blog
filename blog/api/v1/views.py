7a8e5c6f143c42dde2572cddd1601e1d122a4560
MissMohamadi <169094969+MissMohamadi@users.noreply.github.com>
2026-06-27 13:41:52 +0430

    feat: add serializers -create and post and put api views

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,16 +1,36 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 from .serializers import PostSerializer
 from ...models import Post
 
-@api_view()
+@api_view(["GET", "POST"])
 def postList(request):
-    return Response("ok")
+    if request.method == "GET":
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view()
+@api_view(["GET", "PUT"])
 def postDetail(request,id):
-    post = Post.objects.get(id=id)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    post = get_object_or_404(Post, pk=id)
+    if request.method == "GET":
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
 
